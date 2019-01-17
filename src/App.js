38ae4c9c7abdd90cff3401fdedc8819b2233eaef
Alex Kale <alex.kale@cision.com>
2019-01-17 15:56:16 -0500

    edits

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
+  state = {
+    users: {}
+  };
+
+  componentDidMount() {
+    console.log('fu');
+    fetch(
+      'https://hbc-frontend-challenge.hbccommon.private.hbc.com/coffee-week/users'
+    )
+    .then(response => response.json())
+    .then(users => this.setState({ users: users.users }));
+  }
+
+  fetchUsers(endpoint) {
+    return fetch(endpoint)
+      .then(response => response.json())
+      .catch(error => console.warn(error));
+  }
+
+  getUsers() {
+    this.fetchUsers(
+      'https://hbc-frontend-challenge.hbccommon.private.hbc.com/coffee-week/users'
+    ).then(users => {
+      if (users.length) {
+        this.setState({ users });
+      }
+    });
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>YO</h1>
+        <ol>
+          {this.state.users.map(user => {
+            return <li>{user.name.first}</li>;
+          })}
+        </ol>
       </div>
     );
   }
